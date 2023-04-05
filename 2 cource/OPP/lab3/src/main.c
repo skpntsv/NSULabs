d8a8398f16107df21eca61098eec56ed81f22e46
nikita <n.skopintsev@g.nsu.ru>
2023-04-05 23:07:57 +0700

    make init matrix

--- 2 cource/OPP/lab3/src/main.c
+++ 2 cource/OPP/lab3/src/main.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include <mpi.h>
 
 #define N 10
-#define p1 2
-#define p2 2
+#define n1 5
+#define n2 5
+#define n3 5
 
 void multMatrixs(const double* A, const double* x, double* Ax) {
 	for (size_t i = 0; i < N; ++i) {
@@ -16,22 +18,53 @@ void multMatrixs(const double* A, const double* x, double* Ax) {
 	}
 }
 
+void init(double* A, double* B, int rank) {
+	// заполняем блоки матриц случайными значениями
+    srand(rank);
+    for (int i = 0; i < n1 * n2; i++) {
+        A[i] = (double)rand() / RAND_MAX;
+    }
+    for (int i = 0; i < n2 * n3; i++) {
+        B[i] = (double)rand() / RAND_MAX;
+    }
+}
+
 int main(int argc, char *argv[]) {
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	double* A = (double*)calloc(N * N, sizeof(double));
-	double* B = (double*)calloc(N * N, sizeof(double));
-	double* C = (double*)calloc(N * N, sizeof(double));	
-	perror("smthg");
+    if (size % 2 != 0) {
+        if (rank == 0) {
+            printf("The number of processes must be a perfect square\n");
+        }
+        MPI_Finalize();
+        return 0;
+    }
+
+    double* C = (double*)calloc(n1 * n3, sizeof(double));
+	if (rank == 0) {
+		double* A = (double*)calloc(n1 * n2, sizeof(double));
+		double* B = (double*)calloc(n2 * n3, sizeof(double));
+
+		init(A, B, rank);
+	}
+	double* A_local = (double*)calloc(N * N, sizeof(double));
+	double* B_local = (double*)calloc(N * N, sizeof(double));
+	double* C_local = (double*)calloc(N * N, sizeof(double));
 
 	// создаем 2D решетку процессоров
-    int dims[2] = {p1, p2};
+	int sqrt_size = (int)sqrt(size);
+    int dims[2] = {sqrt_size, sqrt_size};
     int periods[2] = {1, 1};
-    MPI_Comm cart_comm;
-    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm);
+    //MPI_Comm cart_comm;
+    //MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm);
+
+    // распределение матриц A и B по процессам
+    countA = n1 / p1;
+    countB = n3 / p2;
+    countC = countA * countB;
 
 
 	free(A);
