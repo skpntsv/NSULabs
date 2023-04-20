968a90181e202c272b64c23ca15ea5e3e1947ab5
nikita <n.skopintsev@g.nsu.ru>
2023-04-20 20:20:27 +0700

    make gatherv

--- 2 cource/OPP/lab3/src/main.c
+++ 2 cource/OPP/lab3/src/main.c
@@ -6,9 +6,12 @@
 #include <mpi.h>
 
 #define N 10
-#define n1 6
-#define n2 6
-#define n3 6
+#define n1 4
+#define n2 4
+#define n3 4
+
+#define X 0
+#define Y 1
 
 void printMatrix(const double* A, int rows, int cols) {
 	for (size_t i = 0; i < rows; ++i) {
@@ -61,9 +64,9 @@ void transposeMatrix(double *A, int rows, int cols) {
 	memcpy(tmp, A, rows * cols * sizeof(double));
 
 	for (int i = 0; i < rows; i++) {
-	for (int j = 0; j < cols; j++) {
-	  A[j * rows + i] = tmp[i * cols + j];
-	}
+		for (int j = 0; j < cols; j++) {
+		  A[j * rows + i] = tmp[i * cols + j];
+		}
 	}
 
 	free(tmp);
@@ -72,32 +75,38 @@ void transposeMatrix(double *A, int rows, int cols) {
 void scatterMatrix(double* A, double** A_local, int rows, int cols, int p, int rank, int size, int coord, int *count, MPI_Comm comm) {
 	(*A_local) = (double*)calloc(rows / p * n2, sizeof(double));
 	if (coord == 0) {
-	    int* displs = (int*)malloc((size + 1) * sizeof(int));
-	    int* scounts = (int*)malloc((size + 1) * sizeof(int));
-	    int offset = 0, extra_cols = rows % p;
-
-	    for (size_t i = 0; i < size; i++) {
-	        displs[i] = offset;
-	        scounts[i] = (rows / p);
-	        
-	        if (i < extra_cols) {
-	            scounts[i]++;
-	            if (i == rank) {
-	                (*count)++;
-	            }
-	        }
-	        
-	        offset += scounts[i] * cols;
-	        scounts[i] *= cols;
-	    }
-
-	    //(*A_local) = (double*)calloc(scounts[rank], sizeof(double));
-	    //printf("scounts[%d] = %d\n", rank, scounts[rank]);
-		MPI_Scatterv(A, scounts, displs, MPI_DOUBLE, *A_local, scounts[rank], MPI_DOUBLE, 0, comm);
-		free(displs);
-		free(scounts);
-		//MPI_Scatter(A, rows / p * cols, MPI_DOUBLE, *A_local, rows / p * cols, MPI_DOUBLE, 0, comm);
+		MPI_Scatter(A, rows / p * cols, MPI_DOUBLE, *A_local, rows / p * cols, MPI_DOUBLE, 0, comm);
 	}
+}
+
+void gatherMatrix(const double *C_local, double *C, int A_block_size, int B_block_size, int size, MPI_Comm comm) {
+    MPI_Datatype not_resized_recv_t;
+    MPI_Datatype resized_recv_t;
+
+    int dims_x = n1 / A_block_size;
+    int dims_y = n3 / B_block_size;
+    int *recv_counts = malloc(sizeof(int) * size);
+    int *displs = malloc(sizeof(int) * size);
+
+    MPI_Type_vector(A_block_size, B_block_size, n3, MPI_DOUBLE, &not_resized_recv_t);
+    MPI_Type_commit(&not_resized_recv_t);
+
+    MPI_Type_create_resized(not_resized_recv_t, 0, B_block_size * sizeof(double), &resized_recv_t);
+    MPI_Type_commit(&resized_recv_t);
+
+    for (int i = 0; i < dims_x; ++i)
+        for (int j = 0; j < dims_y; ++j)
+        {
+            recv_counts[i * dims_y + j] = 1;
+            displs[i * dims_y + j] = j + i * dims_y * A_block_size;
+        }
+
+    MPI_Gatherv(C_local, A_block_size * B_block_size, MPI_DOUBLE, C, recv_counts, displs, resized_recv_t, 0, comm);
+
+    MPI_Type_free(&not_resized_recv_t);
+    MPI_Type_free(&resized_recv_t);
+    free(recv_counts);
+    free(displs);
 }
 
 int main(int argc, char *argv[]) {
@@ -126,11 +135,11 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_rank(comm2d, &rank);
 
 	MPI_Cart_get(comm2d, 2, dims, periods, coords);// получение координат в двумерной решетке
-  	rankX = coords[0]; rankY = coords[1];
+  	rankX = coords[X]; rankY = coords[Y];
 
     // MPI_Comm_split разделит коммуникатор comm2d на непересекающиеся субкоммуникаторы
-    MPI_Comm_split(comm2d, coords[1], coords[0], &commColumns);
-    MPI_Comm_split(comm2d, coords[0], coords[1], &commRows);
+    MPI_Comm_split(comm2d, coords[Y], coords[X], &commColumns);
+    MPI_Comm_split(comm2d, coords[X], coords[Y], &commRows);
 
     double *A = NULL;
     double *B = NULL;
@@ -141,7 +150,6 @@ int main(int argc, char *argv[]) {
 		C = (double*)calloc(n1 * n3, sizeof(double));
 
 		initMatrixs(A, B, rank);
-		//printMatrix(B, n2, n3);
 		transposeMatrix(B, n2, n3);
 
 		printMatrix(A, n1, n2);
@@ -153,18 +161,15 @@ int main(int argc, char *argv[]) {
     B_block_size = n3 / sizeY; //p2
     double* A_local = NULL;
 	double* B_local = NULL;
+	double* C_local = (double*)calloc(A_block_size * B_block_size, sizeof(double));
 
 
 	// 1. Матрица А распределяется по горизонтальным полосам вдоль координаты (x,0).
-	scatterMatrix(A, &A_local, n1, n2, sizeX, rank, size, coords[1], &A_block_size, commColumns);
+	scatterMatrix(A, &A_local, n1, n2, sizeX, rank, size, coords[Y], &A_block_size, commColumns);
 
 	// 2. Матрица B распределяется по вертикальным полосам вдоль координаты (0,y).
-	scatterMatrix(B, &B_local, n3, n2, sizeY, rank, size, coords[0], &B_block_size, commRows);
+	scatterMatrix(B, &B_local, n3, n2, sizeY, rank, size, coords[X], &B_block_size, commRows);
 	transposeMatrix(B_local, B_block_size, n2);
-
-	if (rank == 1) {
-		printMatrix(B_local, n2, B_block_size);
-	}
 
 	// 3. Полосы А распространяются в измерении y
 	MPI_Bcast(A_local, A_block_size * n2, MPI_DOUBLE, 0, commRows);
@@ -172,46 +177,24 @@ int main(int argc, char *argv[]) {
 	// 4. Полосы B распространяются в измерении х
 	MPI_Bcast(B_local, B_block_size * n2, MPI_DOUBLE, 0, commColumns);
 
-	// Выделяем память по матрицу C_local
-	double* C_local = (double*)calloc(A_block_size * B_block_size, sizeof(double));
-
 	// 5. Каждый процесс вычисляет одну подматрицу произведения.
-	//transposeMatrix(B_local, n2, B_block_size);
 	multMatrixs(C_local, A_local, B_local, A_block_size, B_block_size);
-	//transposeMatrix(B_local, B_block_size, n2);
-	//transposeMatrix(C_local, A_block_size, B_block_size);
 
 	if (rank == 0) {
 		//transposeMatrix(C, n1, n3);
 	    printMatrix(C_local, A_block_size, B_block_size);
 	}
 
-	// ТУТ СДЕЛАТЬ МПАЙ ТАЙП
-	MPI_Gather(C_local, A_block_size * B_block_size, MPI_DOUBLE, C, A_block_size * B_block_size, MPI_DOUBLE, 0, comm2d);
+	// 6. Матрица C собирается из (x,y) плоскости.
+	gatherMatrix(C_local, C, A_block_size, B_block_size, size, comm2d);
 
-	if (rank == 0) {
-		//transposeMatrix(C, n1, n3);
-	    printMatrix(C, n1, n3);
-	}
-
-    //printf("rank = %d\n", rank);
     if (rank == 0) {
+    	printMatrix(C, n1, n3);
 		free(A);
 		free(B);
 		free(C);
-		//printMatrix(A, n1, n2);
-		//printMatrix(B, n2, n3);
-		//printf("%lf ", A[1]);
-		//printMatrix(A_local, A_block_size, n2);
-		
-		//printMatrix(B_local, n2, B_block_size);
 	}
-	if (rank == 0) {
-		//printf("rank = %d\n", rank);
 
-		//printMatrix(A_local, A_block_size, n2);
-		//printMatrix(B_local, n2, B_block_size);
-	}
 	free(A_local);
 	free(B_local);
 	free(C_local);
