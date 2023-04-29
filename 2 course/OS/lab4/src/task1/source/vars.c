81da855d63fb607f3054a09659633542f117719c
nikita <n.skopintsev@g.nsu.ru>
2023-04-29 13:02:28 +0700

    modifi

--- 2 course/OS/lab4/src/task1/source/vars.c
+++ 2 course/OS/lab4/src/task1/source/vars.c
@@ -7,28 +7,20 @@
 int global_not_inited[8192];
 int global_inited = 10;
 
-const int global_const;
-
-//char *global_pstr = "global hello";
+const int global_const = 5;
 
 void f() {
   int local;
   static int static_not_inited;
   static int static_inited = 5;
   
-  //char *local_pstr = "local hello";
-  
   const int local_const = 30;
-  
-  //char local_array[] = "local hello array";
   
   printf("\nfunction variables:\n");
   printf("local:             %p (%d)\n", &local, local);
   printf("static inited:     %p (%d)\n", &static_inited, static_inited);
   printf("static not inited: %p (%d)\n", &static_not_inited, static_not_inited);
-  //printf("local pstr:        %p %p (%s)\n", &local_pstr, local_pstr, local_pstr);
   printf("local const        %p (%d)\n", &local_const, local_const);
-  //printf("local array:       %p %p (%s)\n", &local_array, &local_array[5], local_array);
 }
 
 int *f2() {
@@ -61,15 +53,15 @@ void heap() {
 
   printf("\nHEAP: %s", buffer);
 
-  // buffer = malloc(100);
-  // if (buffer == NULL) {
-  //   perror("malloc");
-  // }
-  // strcpy(buffer, "Hello world");
-  // printf("\nHEAP: %s", buffer);
-  // buffer += 50;
-  // free(buffer);
-  // printf("\nHEAP: %s", buffer);
+  buffer = malloc(100);
+  if (buffer == NULL) {
+    perror("malloc");
+  }
+  strcpy(buffer, "Hello world");
+  printf("\nHEAP: %s", buffer);
+  buffer += 50;
+  free(buffer);
+  printf("\nHEAP: %s", buffer);
 }
 
 int main() {
@@ -79,7 +71,6 @@ int main() {
   printf("\nglobal variables:\n");
   printf("global ininted:    %p (%d)\n", &global_inited, global_inited);
   printf("global not inited: %p %p (%d)\n", &global_not_inited, &global_not_inited[8100], global_not_inited[8100]);
-  //printf("global pstr:       %p %p (%s)\n", &global_pstr, global_pstr, global_pstr);
   printf("global const       %p (%d)\n", &global_const, global_const);
   
   f();
@@ -90,10 +81,10 @@ int main() {
   //printf("Value of local from func: %d\n", *local2); 
 
   // Переменая окружения
-  environ();
+  //environ();
 
   // Работа с кучей
-  heap();
+  //heap();
 
   printf("\npid: %d\n", getpid());
   sleep(30);
