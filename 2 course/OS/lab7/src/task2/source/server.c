cea9befc93ad685a0037066f3fe5caf2fbcd9e48
nikita <n.skopintsev@g.nsu.ru>
2023-05-17 01:17:43 +0700

    make TCP server

--- 2 course/OS/lab7/src/task2/source/server.c
+++ 2 course/OS/lab7/src/task2/source/server.c
@@ -4,41 +4,53 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+
+#define BUFFER_SIZE 1024
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8888
 
-void handle_client(int client_socket) {
-    char buffer[1024];
+struct Client {
+    int socket;
+    struct sockaddr_in address;
+};
+
+void handle_client(struct Client* client) {
+    char buffer[BUFFER_SIZE];
     ssize_t bytes_read;
 
-    // Чтение данных от клиента
-    bytes_read = read(client_socket, buffer, sizeof(buffer));
-    if (bytes_read <= 0) {
-        perror("Ошибка чтения данных от клиента");
-        close(client_socket);
-        exit(1);
-    }
+    printf("Новое подключение от клиента: %s:%d\n",
+        inet_ntoa(client->address.sin_addr), ntohs(client->address.sin_port));
 
-    // Отправка данных обратно клиенту
-    if (write(client_socket, buffer, bytes_read) != bytes_read) {
-        perror("Ошибка отправки данных клиенту");
-        close(client_socket);
-        exit(1);
-    }
+    while (1) {
+        // Чтение данных от клиента
+        bytes_read = read(client->socket, buffer, sizeof(buffer));
+        if (bytes_read <= 0) {
+            printf("Клиент отключился: %s:%d\n",
+                inet_ntoa(client->address.sin_addr), ntohs(client->address.sin_port));
+            close(client->socket);
+            exit(0);
+        }
 
-    close(client_socket);
-    exit(0);
+        // Отправка данных обратно клиенту
+        if (write(client->socket, buffer, bytes_read) != bytes_read) {
+            perror("Ошибка отправки данных клиенту");
+            close(client->socket);
+            exit(1);
+        }
+    }
 }
 
 int main() {
-    int server_socket, client_socket;
+    int serverSocket, clientSocket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
 
     // Создание TCP-сокета
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
         perror("Ошибка создания сокета");
         exit(1);
     }
@@ -46,20 +58,20 @@ int main() {
     // Настройка адреса сервера
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
     server_addr.sin_port = htons(SERVER_PORT);
 
     // Привязка сокета к заданному ip и порту
-    if (bind(server_socket, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1) {
+    if (bind(serverSocket, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1) {
         perror("Ошибка привязки сокета");
-        close(server_socket);
+        close(serverSocket);
         exit(1);
     }
 
     // Прослушивание входящих соединений
-    if (listen(server_socket, 10) == -1) {
+    if (listen(serverSocket, 10) == -1) {
         perror("Ошибка прослушивания сокета");
-        close(server_socket);
+        close(serverSocket);
         exit(1);
     }
 
@@ -67,10 +79,10 @@ int main() {
 
     while (1) {
         // Принятие входящего соединения
-        client_socket = accept(server_socket, (struct sockaddr*) &client_addr, &addr_len);
-        if (client_socket == -1) {
+        clientSocket = accept(serverSocket, (struct sockaddr*) &client_addr, &addr_len);
+        if (clientSocket == -1) {
             perror("Ошибка при принятии соединения");
-            close(server_socket);
+            close(serverSocket);
             exit(1);
         }
 
@@ -78,19 +90,25 @@ int main() {
         pid_t pid = fork();
         if (pid == -1) {
             perror("Ошибка создания процесса");
-            close(client_socket);
-            close(server_socket);
+            close(clientSocket);
+            close(serverSocket);
             exit(1);
         } else if (pid == 0) {
             // Мы находимся в дочернем процессе
-            close(server_socket);
-            handle_client(client_socket);
+            close(serverSocket);
+            struct Client client;
+            client.socket = clientSocket;
+            client.address = client_addr;
+            handle_client(&client);
         } else {
             // Мы находимся в родительском процессе
-            close(client_socket);
+            close(clientSocket);
+            if (waitpid(pid, NULL, WNOHANG) == -1) {
+                perror("waitpid");
+                exit(1);
+            }
         }
     }
-
-    close(server_socket);
+    close(serverSocket);
     return 0;
 }
