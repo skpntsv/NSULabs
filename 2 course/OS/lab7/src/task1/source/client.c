e61304f1a5c2e6cdb177a53eaf11a90d6f63c6bd
nikita <n.skopintsev@g.nsu.ru>
2023-05-17 01:18:07 +0700

    make UDP client

--- 2 course/OS/lab7/src/task1/source/client.c
+++ 2 course/OS/lab7/src/task1/source/client.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
 
-#define MAX_BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024
+
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8888
 
@@ -11,18 +13,18 @@ int main() {
     int clientSocket;
     struct sockaddr_in serverAddr;
     socklen_t addrLen = sizeof(struct sockaddr_in);
-    char buffer[MAX_BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
 
     // Создание UDP сокета
     clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
     if (clientSocket == -1) {
         perror("Ошибка при создании сокета");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
     // Настройка адреса сервера
     memset(&serverAddr, 0, sizeof(struct sockaddr_in));
-    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_family = AF_INET; // IPv4
     serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
     serverAddr.sin_port = htons(SERVER_PORT);
 
@@ -31,27 +33,29 @@ int main() {
         fgets(buffer, sizeof(buffer), stdin);
 
         // Удаление символа новой строки ('\n') из введенной строки
-        size_t length = strlen(buffer);
-        if (buffer[length - 1] == '\n') {
-            buffer[length - 1] = '\0';
+        size_t len = strlen(buffer);
+        if (buffer[len - 1] == '\n') {
+            buffer[len - 1] = '\0';
         }
+        
+        // Выход из цикла, если введена команда "q"
+        if (strcmp(buffer, "q") == 0) {
+            break;
+        }
+
         // Отправка данных на сервер
         ssize_t sentBytes = sendto(clientSocket, buffer, strlen(buffer), 0, (struct sockaddr*) &serverAddr, addrLen);
         if (sentBytes == -1) {
             perror("Ошибка при отправке данных на сервер");
-            exit(EXIT_FAILURE);
-        }
-
-        // Выход из цикла, если введена команда "q"
-        if (strcmp(buffer, "q") == 0) {
-            break;
+            exit(1);
         }
 
         // Получение данных от сервера
         ssize_t receivedBytes = recvfrom(clientSocket, buffer, sizeof(buffer), 0, NULL, NULL);
         if (receivedBytes == -1) {
             perror("Ошибка при получении данных от сервера");
-            exit(EXIT_FAILURE);
+            close(clientSocket);
+            exit(1);
         }
 
         // Вывод полученных данных
@@ -59,7 +63,7 @@ int main() {
     }
 
     // Закрытие сокета
-    pclose(clientSocket);
+    close(clientSocket);
 
     return 0;
 }
