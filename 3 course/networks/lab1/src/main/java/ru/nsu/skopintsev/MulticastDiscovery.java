38ccda40c3517fbd42db350aa5f58753952d67b0
skpntsv <n.skopintsev@g.nsu.ru>
2023-09-15 10:27:43 +0700

    more shedule

--- 3 course/networks/lab1/src/main/java/ru/nsu/skopintsev/MulticastDiscovery.java
+++ 3 course/networks/lab1/src/main/java/ru/nsu/skopintsev/MulticastDiscovery.java
@@ -4,15 +4,16 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.net.*;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MulticastDiscovery {
     private final String multicastGroupAddress;
     private final int multicastPort;
     private final MudakTable mudakTable;
-    private final Timer timer = new Timer();
-    private DatagramSocket senderSocket;
+    private final DatagramSocket senderSocket;
+    private ScheduledExecutorService scheduler;
 
     public MulticastDiscovery(String multicastGroupAddress, int multicastPort) {
         this.multicastGroupAddress = multicastGroupAddress;
@@ -23,7 +24,8 @@ public class MulticastDiscovery {
         try {
             this.senderSocket = new DatagramSocket(); // Инициализация сокса для отправки
         } catch (SocketException e) {
-            System.err.println("Create datagram socker failed " + e.getMessage());
+            System.err.println("Create datagram socket failed " + e.getMessage());
+            throw new RuntimeException("Failed to create DatagramSocket", e);
         }
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
@@ -31,66 +33,27 @@ public class MulticastDiscovery {
             sendCommand(MessageType.LEAVE);
         }));
 
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                mudakTable.printTable();
-            }
-        }, 1000, 900);
+        setScheduler();
     }
 
     public void start() {
-            try (MulticastSocket receiveSocket = new MulticastSocket(multicastPort)) {
-            InetAddress group = InetAddress.getByName(multicastGroupAddress);
-            receiveSocket.joinGroup(group);
-
-            Thread senderThread = new Thread(this::sendMulticast);
-            Thread receiverThread = new Thread(this::receiveMulticast);
-
-            senderThread.start();
-            receiverThread.start();
-
-            senderThread.join();
-            receiverThread.join();
-                
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void sendMulticast() {
-        try {
-            while (true) {
-                sendCommand(MessageType.REPORT);
-                Thread.sleep(10000);
-            }
-        } catch (InterruptedException e) {
-            System.err.println("sendMulticast interrupted: " + e.getMessage());
-        } catch (Exception e) {
-            System.err.println("sendMulticast error: " + e.getMessage());
-        }
-    }
-
-    public void sendCommand(@NotNull MessageType commandType) {
-        try {
-            InetAddress group = InetAddress.getByName(multicastGroupAddress);
-            byte[] message = { (byte) commandType.getValue() };
-            DatagramPacket packet = new DatagramPacket(message, message.length, group, multicastPort);
-            senderSocket.send(packet);
-        } catch (IOException e) {
-            System.err.println("sendCommand error: " + e.getMessage());
-        }
-    }
-
-    private void receiveMulticast() {
         try (MulticastSocket receiveSocket = new MulticastSocket(multicastPort)) {
             InetAddress group = InetAddress.getByName(multicastGroupAddress);
             receiveSocket.joinGroup(group);
 
-            byte[] buffer = new byte[1];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            receiverMulticast(receiveSocket);
 
-            while (true) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void receiverMulticast(MulticastSocket receiveSocket) {
+        byte[] buffer = new byte[1];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+        while (true) {
+            try {
                 receiveSocket.receive(packet);
                 byte receivedMessageType = packet.getData()[0];
                 String user = packet.getAddress().getHostAddress() + ":" + packet.getPort();
@@ -110,9 +73,35 @@ public class MulticastDiscovery {
                     }
                 }
                 mudakTable.killZombie();
+            } catch (IOException e) {
+                System.err.println("receiveMulticast error: " + e.getMessage());
             }
-        } catch (IOException e) {
-            System.err.println("receiveMulticast error: " + e.getMessage());
         }
+    }
+
+    private void sendMulticast() {
+        sendCommand(MessageType.REPORT);
+    }
+
+    public void sendCommand(@NotNull MessageType commandType) {
+        try {
+            InetAddress group = InetAddress.getByName(multicastGroupAddress);
+            byte[] message = { (byte) commandType.getValue() };
+            DatagramPacket packet = new DatagramPacket(message, message.length, group, multicastPort);
+            senderSocket.send(packet);
+        } catch (IOException e) {
+            System.err.println("sendCommand error: " + e.getMessage());
+        }
+    }
+
+    private void setScheduler() {
+        scheduler = Executors.newScheduledThreadPool(2);
+
+        scheduler.scheduleAtFixedRate(this::printTable, 1000, 900, TimeUnit.MILLISECONDS);
+        scheduler.scheduleAtFixedRate(this::sendMulticast, 1, 10, TimeUnit.SECONDS);
+    }
+
+    private void printTable() {
+        mudakTable.printTable();
     }
 }
