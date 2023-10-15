de94e8ec94a936ae59de9787bc3f4369b346bbca
skpntsv <n.skopintsev@g.nsu.ru>
2023-10-16 00:06:27 +0700

    update

--- 3 course/os/lab1/task1/src/5.c
+++ 3 course/os/lab1/task1/src/5.c
@@ -9,79 +9,72 @@
 #include <signal.h>
 
 
-void *thread_1(void* arg){
-	sigset_t set;
+void *thread_1(void* arg) {
+    sigset_t set;
     if (sigfillset(&set) == -1) {
         perror("thread_1: sigfillset() failed");
         return NULL;
     }
-	if (pthread_sigmask(SIG_BLOCK, &set, NULL)) {
+    if (pthread_sigmask(SIG_BLOCK, &set, NULL)) {
         perror("thread_1: pthread_sigmask() failed");
         return NULL;
     }
 
-	printf("thread_1: first thread blocked signals\n");
+    printf("thread_1: thread blocked signals\n");
 
-	sleep(100);
-	printf("thread_1: first thread has finished its work\n");
-	return NULL;
+    sleep(100);
+    printf("thread_1: thread has finished\n");
+    return NULL;
 }
 
-void sigint_handler(int sig){
-	printf("Signal received: %d\n", sig);
+void sigint_handler(int sig) {
+    printf("Signal received: %d\n", sig);
 }
 
-void *thread_2(void * arg){
-	signal(SIGINT, sigint_handler);
+void *thread_2(void * arg) {
+    if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+        perror("thread_2: signal() failed");
+        return NULL;
+    }
 
-	return NULL;
+    return NULL;
 }
 
-void *thread_3(void * arg){
-	int sig;
-	
-	sigset_t set;
-	if (sigemptyset(&set) == -1) {
+void *thread_3(void * arg) {
+    int sig;
+
+    sigset_t set;
+    if (sigemptyset(&set) == -1) {
         perror("thread_3: sigemptyset() failed");
         return NULL;
     }
-	if (sigaddset(&set, SIGQUIT) == -1) {
+    if (sigaddset(&set, SIGQUIT) == -1) {
         perror("thread_3: sigaddset failed");
         return NULL;
     }
 
-	pthread_sigset(SIG_BLOCK, &set, NULL);
-	
-	printf("thread_3: third thread is waitng a SIGQUIT signal\n");
-	
-	sigwait(&set,&sig);
-	
-	printf("thread_3: third thread received SIGQUIT signal: %d\n",sig);
+    printf("thread_3: thread is waiting for a SIGQUIT signal\n");
 
-	return NULL;
+    if (sigwait(&set, &sig) != 0) {
+        perror("thread_3: sigwait() failed");
+        return NULL;
+    }
+
+    printf("thread_3: thread received SIGQUIT signal: %d\n", sig);
+
+    return NULL;
 }
 
-
-int main(){
-    pthread_t tid1; 
+int main() {
+    pthread_t tid1;
     pthread_t tid2;
     pthread_t tid3;
 
-	printf("Pid:%d\n",getpid());
+    printf("main: pid:%d\n", getpid());
 
-    pthread_create(&tid1, NULL, thread_1, NULL);	
+    pthread_create(&tid1, NULL, thread_1, NULL);
     pthread_create(&tid2, NULL, thread_2, NULL);
     pthread_create(&tid3, NULL, thread_3, NULL);
-
-   pthread_kill(tid1,SIGINT);
-   sleep(2);
-   pthread_kill(tid2,SIGINT);
-   sleep(2);
-   pthread_kill(tid3,SIGQUIT);
-   sleep(2);
-
-   // останавливать с помощью консоли (на клавиатуре)
-
 
     pthread_exit(0);
     return 0;
