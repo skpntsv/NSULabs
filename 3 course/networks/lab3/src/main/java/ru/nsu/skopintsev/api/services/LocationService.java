3502253efa3a438cb7897c09f098c72558cc0717
skpntsv <n.skopintsev@g.nsu.ru>
2023-11-10 23:43:22 +0700

    isolated the work of Api

--- 3 course/networks/lab3/src/main/java/ru/nsu/skopintsev/api/services/LocationService.java
+++ 3 course/networks/lab3/src/main/java/ru/nsu/skopintsev/api/services/LocationService.java
@@ -1,29 +1,16 @@
 package ru.nsu.skopintsev.api.services;
 
-import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 import okhttp3.ResponseBody;
 import com.google.gson.Gson;
-import org.jetbrains.annotations.NotNull;
 import ru.nsu.skopintsev.api.responses.LocationResponse;
 
 import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
 
 public class LocationService {
     private static final String API_URL = "https://graphhopper.com/api/1/geocode";
-    private final OkHttpClient httpClient;
-    private final Gson gson;
 
-    public LocationService() {
-        httpClient = new OkHttpClient();
-        gson = new Gson();
-    }
-
-    public CompletableFuture<LocationResponse.Location[]> searchLocations(String query) {
-        CompletableFuture<LocationResponse.Location[]> future = new CompletableFuture<>();
-
+    public Request getRequest(String query) {
         
         String apiUrl = API_URL + "?q=" + query + "&key=" + apiKey;
 
@@ -32,29 +19,15 @@ public class LocationService {
                 .get()
                 .build();
 
-        httpClient.newCall(request).enqueue(new okhttp3.Callback() {
-            @Override
-            public void onResponse(@NotNull okhttp3.Call call, @NotNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    ResponseBody responseBody = response.body();
-                    if (responseBody != null) {
-                        String json = responseBody.string();
-                        LocationResponse locationResponse = gson.fromJson(json, LocationResponse.class);
-                        future.complete(locationResponse.getHits());
-                    } else {
-                        future.completeExceptionally(new IOException("Empty response body"));
-                    }
-                } else {
-                    future.completeExceptionally(new IOException("Request failed with status code: " + response.code()));
-                }
-            }
+        return request;
+    }
 
-            @Override
-            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
-                future.completeExceptionally(e);
-            }
-        });
+    public LocationResponse responseBodyToLocationResponse(ResponseBody responseBody) throws IOException {
+        String json = responseBody.string();
 
-        return future;
+        Gson gson = new Gson();
+        LocationResponse locationResponse = gson.fromJson(json, LocationResponse.class);
+
+        return locationResponse;
     }
 }
--- 3 course/networks/lab3/src/main/java/ru/nsu/skopintsev/controller/LocationController.java
+++ 3 course/networks/lab3/src/main/java/ru/nsu/skopintsev/controller/LocationController.java
@@ -1,18 +1,49 @@
 package ru.nsu.skopintsev.controller;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.jetbrains.annotations.NotNull;
 import ru.nsu.skopintsev.api.responses.LocationResponse;
 import ru.nsu.skopintsev.api.services.LocationService;
 
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
 public class LocationController {
     private final LocationService locationService;
+    private final OkHttpClient httpClient;
 
     public LocationController() {
+        this.httpClient = new OkHttpClient();
         this.locationService = new LocationService();
     }
 
     public CompletableFuture<LocationResponse.Location[]> searchLocations(String query) {
-        return locationService.searchLocations(query);
+        CompletableFuture<LocationResponse.Location[]> future = new CompletableFuture<>();
+
+        httpClient.newCall(locationService.getRequest(query)).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onResponse(@NotNull okhttp3.Call call, @NotNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    ResponseBody responseBody = response.body();
+                    if (responseBody != null) {
+                        LocationResponse locationResponse = locationService.responseBodyToLocationResponse(responseBody);
+                        future.complete(locationResponse.getHits());
+                    } else {
+                        future.completeExceptionally(new IOException("Empty response body"));
+                    }
+                } else {
+                    future.completeExceptionally(new IOException("Request failed with status code: " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
+                future.completeExceptionally(e);
+            }
+        });
+
+        return future;
     }
 }
