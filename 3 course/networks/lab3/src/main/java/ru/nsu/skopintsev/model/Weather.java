d3df5abd6081f47803b23fb24f8b4b7b6e5cdf6f
skpntsv <n.skopintsev@g.nsu.ru>
2023-11-11 16:30:18 +0700

    override toString method

--- 3 course/networks/lab3/src/main/java/ru/nsu/skopintsev/model/Weather.java
+++ 3 course/networks/lab3/src/main/java/ru/nsu/skopintsev/model/Weather.java
@@ -4,11 +4,22 @@ import lombok.Data;
 
 @Data
 public class Weather {
-    private Long temperature;
-    private Long feelsLikeTemperature;
-    private Long windSpeed;
-    private String windDirection;
+    private double temperature;
+    private double feelsLikeTemperature;
+    private double windSpeed;
+    private double windDirection;
     private Long pressure;
     private Long humidity;
     private String desc;
+
+    @Override
+    public String toString() {
+        return "Temperature: " + temperature + " °C\n" +
+                "Feels Like Temperature: " + feelsLikeTemperature + " °C\n" +
+                "Wind Speed: " + windSpeed + " m/s\n" +
+                "Wind Direction: " + windDirection + "°\n" +
+                "Pressure: " + pressure + " hPa\n" +
+                "Humidity: " + humidity + "%\n" +
+                "Description: " + desc + "\n";
+    }
 }
