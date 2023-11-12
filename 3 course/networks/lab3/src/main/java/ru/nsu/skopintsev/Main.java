603b525482795c08f389194872e36d21c30981bd
skpntsv <n.skopintsev@g.nsu.ru>
2023-11-12 22:27:17 +0700

    test

--- 3 course/networks/lab3/src/main/java/ru/nsu/skopintsev/Main.java
+++ 3 course/networks/lab3/src/main/java/ru/nsu/skopintsev/Main.java
@@ -1,15 +1,11 @@
 package ru.nsu.skopintsev;
 
-import ru.nsu.skopintsev.api.responses.PlacesResponse;
-import ru.nsu.skopintsev.controller.LocationController;
-import ru.nsu.skopintsev.controller.PlacesController;
-import ru.nsu.skopintsev.controller.WeatherController;
-import ru.nsu.skopintsev.model.Location;
-import ru.nsu.skopintsev.model.Weather;
+import ru.nsu.skopintsev.controller.*;
+import ru.nsu.skopintsev.model.*;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,44 +17,51 @@ public class Main {
         LocationController locationController = new LocationController();
         CompletableFuture<Location[]> locationFuture = locationController.searchLocations(locationName);
 
-        locationFuture.thenAccept(locations -> {
+        locationFuture.thenCompose(locations -> {
             if (locations.length > 0) {
                 System.out.println("Выберите локацию:");
                 for (int i = 0; i < locations.length; i++) {
-                    System.out.println(i + 1 + ". " + locations[i].toString());
+                    System.out.println(i + 1 + ". " + locations[i]);
                 }
 
                 System.out.print("Введите номер выбранной локации: ");
                 int selectedLocationIndex = Integer.parseInt(scanner.nextLine()) - 1;
+                System.out.println();
                 Location selectedLocation = locations[selectedLocationIndex];
 
                 WeatherController weatherController = new WeatherController();
-                CompletableFuture<Weather> weatherFuture = weatherController.
-                        getWeatherByCoords(selectedLocation.getLat(), selectedLocation.getLng());
+                CompletableFuture<Weather> weatherFuture = weatherController.getWeatherByCoords(
+                        selectedLocation.getLat(), selectedLocation.getLng());
 
-                weatherFuture.thenAccept(weather -> {
-                    System.out.println("Информация о погоде:\n" + weather.toString());
+                PlacesController placesController = new PlacesController();
+                CompletableFuture<String[]> placesFuture = placesController.getInterestingPlaces(
+                        selectedLocation.getLat(), selectedLocation.getLng());
 
-                    PlacesController placesController = new PlacesController();
-                    CompletableFuture<PlacesResponse> placesFuture = placesController.getInterestingPlaces(selectedLocation.getLat(), selectedLocation.getLng());
+                return weatherFuture.thenCombine(placesFuture, (weather, xids) -> {
+                    System.out.println("Погода в " + selectedLocation.getName() + ":");
+                    System.out.println(weather);
 
-                    placesFuture.thenAccept(places -> {
-                        System.out.println("Интересные места в " + selectedLocation.getName() + ":");
-                        for (PlacesResponse.Place place : places.getPlaces()) {
-                            System.out.println(place.toString());
-                        }
-                    });
+                    System.out.println("\nИнтересные места в " + selectedLocation.getName() + ":");
+                    CompletableFuture<Void> allDescriptions = CompletableFuture.allOf(
+                            Arrays.stream(xids).map(xid -> {
+                                PlaceDescriptionController placeDescriptionController = new PlaceDescriptionController();
+                                CompletableFuture<Place> placeFuture = placeDescriptionController.getPlaceDescription(xid);
+
+                                return placeFuture.thenAccept(place -> {
+                                    if (place.getName() != null && place.getDescription() != null) {
+                                        System.out.println(place);
+                                        System.out.println();
+                                    }
+                                });
+                            }).toArray(CompletableFuture[]::new)
+                    );
+
+                    return allDescriptions.thenApply(v -> null);
                 });
             } else {
                 System.out.println("Локации не найдены");
+                return CompletableFuture.completedFuture(null);
             }
-        });
-
-        try {
-            CompletableFuture<Void> allOf = CompletableFuture.allOf(locationFuture);
-            allOf.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
+        }).join();
     }
 }
