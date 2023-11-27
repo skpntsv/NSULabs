13846e06d079df672f5bc72f5e7d550d05daa51f
skpntsv <n.skopintsev@g.nsu.ru>
2023-11-27 19:31:35 +0700

    add random swap of nodes

--- 3 course/os/lab2/task2/src/main.c
+++ 3 course/os/lab2/task2/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "linkedlist.h"
 
@@ -60,7 +61,7 @@ void* descending_length_count(void* arg) {
     return NULL;
 }
 
-void* equal_length_сount(void* arg) {
+void* equal_length_count(void* arg) {
     Storage* storage = (Storage *) arg;
 
     while (1) {
@@ -88,6 +89,28 @@ void* equal_length_сount(void* arg) {
     return NULL;
 }
 
+void* random_swap(void* arg) {
+    Storage* storage = (Storage *) arg;
+
+    while (1) {
+        Node* prev = storage->first;
+        Node* current = prev->next;
+        Node* next = current->next;
+
+        while (next != NULL) {
+            if (rand() % 2 == 0) {
+                swap_nodes(prev, current, next);
+            }
+
+            prev = prev->next;
+            current = prev->next;
+            next = (current != NULL) ? current->next : NULL;
+        }
+    }
+
+    return NULL;
+}
+
 void *count_monitor(void *arg) {
     while (1) {
         printf("Ascending: %d, Descending: %d, Equal: %d\n", 
@@ -98,6 +121,14 @@ void *count_monitor(void *arg) {
     return NULL;
 }
 
+void fill_storage(Storage* storage, int num_nodes) {
+    for (int i = 0; i < num_nodes; ++i) {
+        char buff[99];
+        sprintf(buff, "%d", i);
+        storage_add(storage, buff);
+    }
+}
+
 int main() {
     Storage* storage = storage_init();
     int err;
@@ -105,13 +136,15 @@ int main() {
     storage_add(storage, "Hello");
     storage_add(storage, "World");
     fill_storage(storage, 1000);
-    print_storage(storage);
+    storage_print(storage);
 
-    //sleep(15);
     pthread_t monitor;
     pthread_t equal_thread, ascending_thread, descending_thread;
-
-    err = pthread_create(&equal_thread, NULL, equal_length_сount, storage);
+    pthread_t swap1, swap2, swap3;
+    
+    // ----------------------------------------------------------------------------
+    // EQUAULS FUNCTIONS
+    err = pthread_create(&equal_thread, NULL, equal_length_count, storage);
 	if (err) {
 		printf("main: pthread_create() failed: %s\n", strerror(err));
 		return -1;
@@ -132,6 +165,26 @@ int main() {
 		printf("main: pthread_create() failed: %s\n", strerror(err));
 		return -1;
 	}
+    // EQUAULS FUNCTIONS
+    // ---------------------------------------------------------------------------
+    // SWAP FUNCTIONS
+    err = pthread_create(&swap1, NULL, random_swap, storage);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+    err = pthread_create(&swap2, NULL, random_swap, storage);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+    err = pthread_create(&swap3, NULL, random_swap, storage);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+    // SWAP FUNCTIONS
+    // ----------------------------------------------------------------------------
 
 	if (pthread_join(equal_thread, NULL)) {
 		perror("pthread_join - equal_thread");
@@ -145,6 +198,10 @@ int main() {
     if (pthread_join(monitor, NULL)) {
 		perror("pthread_join - monitor");
 	}
+
+    pthread_join(swap1, NULL);
+    pthread_join(swap2, NULL);
+    pthread_join(swap3, NULL);
 
     storage_destroy(storage);
 
