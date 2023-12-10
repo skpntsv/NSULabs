eb586b5218904fa087dd5ba69f3dbef216dbcd06
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-10 23:21:40 +0700

    make test for mutex

---
+++ 3 course/os/lab2/task3/src/test_my_mutex.c
@@ -0,0 +1,111 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <pthread.h>
+
+#define RED "\033[41m"
+#define NOCOLOR "\033[0m"
+
+#include "../../task1/sync/queue.h"
+#include "../my_mutex_lib/my_mutex.h"
+
+my_mutex_t lock;
+
+void set_cpu(int n) {
+	int err;
+	cpu_set_t cpuset;
+	pthread_t tid = pthread_self();
+
+	CPU_ZERO(&cpuset);
+	CPU_SET(n, &cpuset);
+
+	err = pthread_setaffinity_np(tid, sizeof(cpu_set_t), &cpuset);
+	if (err) {
+		printf("set_cpu: pthread_setaffinity failed for cpu %d\n", n);
+		return;
+	}
+
+	printf("set_cpu: set cpu %d\n", n);
+}
+
+void *reader(void *arg) {
+	int expected = 0;
+	queue_t *q = (queue_t *)arg;
+	printf("reader [%d %d %d]\n", getpid(), getppid(), gettid());
+
+	set_cpu(1);
+
+	while (1) {
+		int val = -1;
+        my_mutex_lock(&lock);
+		int ok = queue_get(q, &val);
+		my_mutex_unlock(&lock);
+		if (!ok)
+			continue;
+
+		if (expected != val)
+			printf(RED"ERROR: get value is %d but expected - %d" NOCOLOR "\n", val, expected);
+
+		expected = val + 1;
+	}
+
+	return NULL;
+}
+
+void *writer(void *arg) {
+	int i = 0;
+	queue_t *q = (queue_t *)arg;
+	printf("writer [%d %d %d]\n", getpid(), getppid(), gettid());
+
+	set_cpu(2);
+
+	while (1) {
+		my_mutex_lock(&lock);
+		int ok = queue_add(q, i);
+		my_mutex_unlock(&lock);
+		if (!ok)
+			continue;
+		i++;
+	}
+
+	return NULL;
+}
+
+int main() {
+	pthread_t tid_reader;
+	pthread_t tid_writer;
+	queue_t *q;
+	int err;
+
+	printf("main [%d %d %d]\n", getpid(), getppid(), gettid());
+
+	q = queue_init(100000);
+    my_mutex_init(&lock);
+
+	err = pthread_create(&tid_reader, NULL, reader, q);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+
+	err = pthread_create(&tid_writer, NULL, writer, q);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		return -1;
+	}
+
+	if (pthread_join(tid_reader, NULL)) {
+		perror("pthread_join - tid_reader");
+	}
+	if (pthread_join(tid_writer, NULL)) {
+		perror("pthread_join - tid_writer");
+	}
+
+	return 0;
+}
