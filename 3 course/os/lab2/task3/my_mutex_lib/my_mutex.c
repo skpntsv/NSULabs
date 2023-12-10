4688ab8b2700bbf55fa1e9f59c666afec2badb66
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-10 23:21:32 +0700

    rework mutex

--- 3 course/os/lab2/task3/my_mutex_lib/my_mutex.c
+++ 3 course/os/lab2/task3/my_mutex_lib/my_mutex.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <stdatomic.h>
@@ -9,45 +10,17 @@
 
 #include "my_mutex.h"
 
-struct my_mutex {
-    int lock;
-};
-
-int futex(atomic_flag *uaddr, int futex_op, int val) {
-    return syscall(SYS_futex, uaddr, futex_op, val, NULL, NULL, 0);
+void my_mutex_init(my_mutex_t *mutex) { 
+    mutex->lock = 0; 
 }
 
-void my_mutex_init(my_mutex_t *m) {
-    m->lock = 1;
-}
-
-void my_mutex_lock(my_mutex_t *m) {
-    int err;
-
-    while (1) {
-        int one = 1;
-
-        if (atomic_compare_exchange_strong(&m->lock, &one, 0)) {
-            break;
-        }
-
-        err = my_futex(&m->lock, FUTEX_WAIT, 0);
-        if (err == -1 && errno != EAGAIN) {
-            printf("futex FUTEX_WAIT failed: %s\n", strerror(errno));
-            abort();
-        }
+void my_mutex_lock(my_mutex_t *mutex) {
+    while (__sync_lock_test_and_set(&mutex->lock, 1) != 0) {
+        syscall(SYS_futex, &mutex->lock, FUTEX_WAIT, 1, NULL, NULL, 0);
     }
 }
 
-void my_mutex_unlock(my_mutex_t *m) {
-    const int zero = 0;
-    if (atomic_compare_exchange_strong(&m->lock, &zero, 1)) {
-        int err;
-
-        err = my_futex(&m->lock, FUTEX_WAIT, 1, NULL, NULL, 0);
-        if (err == -1) {
-            printf("futex FUTEX_WAIT failed: %s\n", strerror(errno));
-            abort();
-        }
-    }
+void my_mutex_unlock(my_mutex_t *mutex) {
+    __sync_lock_release(&mutex->lock);
+    syscall(SYS_futex, &mutex->lock, FUTEX_WAKE, 1, NULL, NULL, 0);
 }
--- 3 course/os/lab2/task3/my_mutex_lib/my_mutex.h
+++ 3 course/os/lab2/task3/my_mutex_lib/my_mutex.h
@@ -1,7 +1,11 @@
 #ifndef MY_MUTEX_H
 #define MY_MUTEXT_H
 
-typedef struct my_mutex my_mutex_t;
+#include <stdatomic.h>
+
+typedef struct my_mutex {
+    int lock;
+} my_mutex_t;
 
 void my_mutex_init(my_mutex_t *m);
 void my_mutex_lock(my_mutex_t *m);
