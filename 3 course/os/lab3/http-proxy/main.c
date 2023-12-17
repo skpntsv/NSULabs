3e9977648f65e627667d6bbe9a843923753ded61
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-17 21:59:44 +0700

    make parse request and send to website

--- 3 course/os/lab3/http-proxy/main.c
+++ 3 course/os/lab3/http-proxy/main.c
@@ -5,8 +5,11 @@
 #include <pthread.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
 
-//#include "proxy.h"
+#include "network_utils.h"
 
 #define PORT 80
 #define LISTENQ 10
@@ -14,29 +17,32 @@
 #define MAX_BUFFER_SIZE 1024*16
 #define CACHE_SIZE 1024*4
 
-typedef struct request_t {
-    char host[MAX_URL_SIZE / 2];
-    char path[MAX_URL_SIZE / 2];
+typedef struct Request_t {
+    char host[MAX_URL_SIZE];
+    char path[MAX_URL_SIZE];
     int port;
 
 } request;
 
-typedef struct cache_t {
+typedef struct Cache_t {
     char url[MAX_URL_SIZE];
     char body[MAX_BUFFER_SIZE];
 
     struct cache *next;
 } cache;
 
-void error() {
-    printf("something went wrong...");
-}
+typedef struct ThreadArgs_t {
+    int client_socket;
+    // TODO добавить кэш
+} threadArgs;
 
 void parse_url(char *url, request *content) {
     char tmp[MAX_BUFFER_SIZE];
     if (strstr(url, "http://") != NULL) {
         sscanf(url, "http://%[^/]%s", tmp, content->path);
-    } // TODO else???
+    } else {
+        sscanf(url, "%[^/]%s", tmp, content->path);
+    }
 
     if (strstr(tmp, ":") != NULL) {
         sscanf(tmp, "%[^:]:%d", content->host, &content->port);
@@ -55,20 +61,20 @@ void* client_handler(void* args) {
 
     char buffer[MAX_BUFFER_SIZE];
     
-    int bytes_read = recv(client_socket, buffer, MAX_BUFFER_SIZE, 0);
+    int bytes_read = receive_request(client_socket, buffer, sizeof(buffer));
     if (bytes_read < 0) {
-        perror("recv");
+        perror("receive_all");
         close(client_socket);
 
         return NULL;
     }
-    printf("buffer = %s\n", buffer);
+    printf("buffer =\n %s\n", buffer);
 
     char method[100], url[MAX_URL_SIZE], version[100];
     sscanf(buffer, "%s %s %s", method, url, version);
 
     if (strcasecmp(method, "GET")) {
-        printf("This proxy doesn't support this method - %s", method);
+        printf("This proxy doesn't support this method - %s\n", method);
     }
 
     request content;
@@ -80,61 +86,46 @@ void* client_handler(void* args) {
 
     // snprintf(new_request, "%sHost: %s\n", new_request, content.host);
 
-    int clientfd;
+    int website_socket = create_socket(content.host, content.port);
 
+    send_request(website_socket, buffer, sizeof(buffer));
+    printf("Send request to website:\n %s\n", buffer);
+
+    char response[MAX_BUFFER_SIZE];
+    receive_request(website_socket, response, sizeof(response));
+    printf("Get request from website:\n%s\n", response);
+
+    close(website_socket);
+
+    send_request(client_socket, response, sizeof(response));
 
     return NULL;
 }
 
-int main() {
+void start_proxy_server() {
     int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
 
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
-        perror("Error creating socket");
-        exit(EXIT_FAILURE);
-    }
-    int opt = 1;
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("setsockopt");
-    }
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(PORT);
-
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Error binding socket");
-        exit(EXIT_FAILURE);
-    }   
-
-    if (listen(server_socket, LISTENQ) < 0) {
-        perror("Error listening");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Proxy server started and listening on port %d\n", PORT);
+    init_server_socket(&server_socket, PORT, LISTENQ);
 
     while (1) {
-        // Принимаем входящее соединение
         client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
         if (client_socket < 0) {
             perror("Error accepting connection");
             continue;
         }
-        //Создаем отдельный поток для обработки HTTP-запроса
+
         pthread_t thread;
         pthread_create(&thread, NULL, client_handler, &client_socket);
         pthread_detach(thread);
-
-        //handle_http_request(client_socket);
     }
 
     close(server_socket);
+}
 
+int main() {
+    start_proxy_server();
 
     return 0;
 }
