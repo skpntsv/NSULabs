012b560e8927586bb643ae3d22ae50804f37c9c6
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-20 10:39:11 +0700

    rework

--- 3 course/os/lab2/task3/my_mutex_lib/my_mutex.c
+++ 3 course/os/lab2/task3/my_mutex_lib/my_mutex.c
@@ -1,26 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <stdatomic.h>
-#include <linux/futex.h>
-#include <sys/syscall.h>
-#include <errno.h>
+#define _GNU_SOURCE
 
 #include "my_mutex.h"
 
-void my_mutex_init(my_mutex_t *mutex) { 
-    mutex->lock = 0; 
+void my_mutex_init(my_mutex_t* m) {
+	m->lock = 1;
 }
 
-void my_mutex_lock(my_mutex_t *mutex) {
-    while (__sync_lock_test_and_set(&mutex->lock, 1) != 0) {
-        syscall(SYS_futex, &mutex->lock, FUTEX_WAIT, 1, NULL, NULL, 0);
-    }
+int futex(int* uaddr, int futex_op, int val,
+	const struct timespec* timeout, int* uaddr2, int val3) {
+
+	return syscall(SYS_futex, uaddr, futex_op, val,
+		timeout, uaddr2, val3);
 }
 
-void my_mutex_unlock(my_mutex_t *mutex) {
-    __sync_lock_release(&mutex->lock);
-    syscall(SYS_futex, &mutex->lock, FUTEX_WAKE, 1, NULL, NULL, 0);
+void my_mutex_lock(my_mutex_t* m) {
+	int err;
+
+	while (1) {
+		int one = 1;
+		if (atomic_compare_exchange_strong(&m->lock, &one, 0))
+			break;
+
+		err = futex(&m->lock, FUTEX_WAIT, 0, NULL, NULL, 0);
+		if (err == -1 && errno != EAGAIN) {
+			perror("futex WAIT");
+			abort();
+		}
+	}
+}
+
+void my_mutex_unlock(my_mutex_t* m) {
+	int zero = 0;
+	if (atomic_compare_exchange_strong(&m->lock, &zero, 1)) {
+		int err;
+
+		err = futex(&m->lock, FUTEX_WAKE, 1, NULL, NULL, 0);
+		if (err == -1) {
+			perror("futex WAIT");
+			abort();
+		}
+	}
 }
--- 3 course/os/lab2/task3/my_mutex_lib/my_mutex.h
+++ 3 course/os/lab2/task3/my_mutex_lib/my_mutex.h
@@ -1,14 +1,23 @@
-#ifndef MY_MUTEX_H
-#define MY_MUTEXT_H
-
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdatomic.h>
+#include <linux/futex.h>
+#include <sys/syscall.h>
 
-typedef struct my_mutex {
-    int lock;
+typedef struct {
+	int lock;
 } my_mutex_t;
 
-void my_mutex_init(my_mutex_t *m);
-void my_mutex_lock(my_mutex_t *m);
-void my_mutex_unlock(my_mutex_t *m);
+void my_mutex_init(my_mutex_t* m);
 
-#endif
+int futex(int* uaddr, int futex_op, int val,
+	const struct timespec* timeout, int* uaddr2, int val3);
+
+void my_mutex_lock(my_mutex_t* m);
+
+void my_mutex_unlock(my_mutex_t* m);
