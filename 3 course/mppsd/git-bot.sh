b93fcc16cde338d7b234be5bb277a07c64c77938
skpntsv <n.skopintsev@g.nsu.ru>
2024-03-23 16:55:08 +0700

    rename file

--- 3 course/mppsd/git-bot.sh
+++ 3 course/mppsd/git-bot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-REPO_PATH="PATH_TO_GIT_REP"
-N_DAYS=150
-
+REPO_PATH="PATH_TO_REP"
+N_DAYS=30
 YOUR_EMAIL="YOUR_EMAIL"
 
+
 cd "$REPO_PATH" || exit
-REPO_NAME=$(basename $REPO_PATH)
+REPO_NAME=$(basename "$(git rev-parse --show-toplevel)")
 
 BRANCHES=$(git branch -r | grep -v 'origin/HEAD' | sed 's/origin\///')
 
@@ -35,12 +35,13 @@ for BRANCH in $BRANCHES; do
         if [[ "$AUTHOR_EMAIL" == *.*@*.* ]]; then
             subject="[$REPO_NAME] - Your branch $BRANCH needs attention"
             body="Hello $AUTHOR_NAME,\n\nYour branch $BRANCH in the repository $REPO_NAME hasn't been updated for $DAYS_SINCE_LAST_COMMIT days. Please review and update it as necessary.\n\nBest regards,\nYour Git Bot"
-            echo -e "$body" | mailx -s "$subject" -r "$YOUR_EMAIL" "$AUTHOR_EMAIL"
+            #echo "Subject: $subject"
+            #echo "Body: $body"
+            echo -e "$body" | mailx -s "$subject" -aFrom:"Git Bot <$YOUR_EMAIL>" "$AUTHOR_EMAIL"
             echo "Email sent to $AUTHOR_EMAIL"
         else
             echo "Invalid email address: $AUTHOR_EMAIL. Skipping..."
         fi
-
         echo "---------------------"
     fi
 done
